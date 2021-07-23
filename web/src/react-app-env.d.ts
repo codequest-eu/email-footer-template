58a3b5c4d8d2dc9c85ff935310c88c1c3d83cc7e
Piotr Kawczyński <piotr.kawczynski@codequest.com>
2021-07-23 16:09:00 +0200

    Update workflow with envs

--- web/src/react-app-env.d.ts
+++ web/src/react-app-env.d.ts
@@ -5,9 +5,6 @@ declare namespace NodeJS {
     /** Application API URL */
     REACT_APP_API_URL?: string;
 
-    /** <title> content */
-    REACT_APP_TITLE?: string;
-
     /** Path prefix, useful when the app is hosted from a directory, eg. /site */
     REACT_APP_BASENAME?: string;
   }
